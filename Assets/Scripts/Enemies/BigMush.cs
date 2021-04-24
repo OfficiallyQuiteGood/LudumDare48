1fbe89d41ef838a6932519949cc3bea6f04994dc
freddypage <fredlepage782@gmail.com>
2021-04-24 11:13:20 -0400

    removed old assets and added enemy functionality

---
+++ Assets/Scripts/Enemies/BigMush.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigMush : Enemy
+{
+    public float timeInterval = 1f;
+    public Animator animator;
+
+    Vector3 startingPosition;
+
+	bool jump = false;
+    int direction = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MoveAtInterval());
+        startingPosition = transform.position;
+    }
+
+    private Vector3 getRoamingPosition()
+    {
+        return startingPosition + new Vector3(10*Enemy.getRandomHorizontalDirection(), 0, 0);
+    }
+
+    protected IEnumerator MoveAtInterval()
+    {
+        
+        while (true)
+        {
+            yield return new WaitForSeconds(timeInterval);
+            //body.velocity = new Vector2(-speed, 0);
+            //Move();
+            shouldMove = !shouldMove;
+            direction = Enemy.getRandomHorizontalDirection();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(shouldMove) transform.position+=new Vector3(direction * movementSpeed * Time.fixedDeltaTime,0,0);
+    }
+
+    virtual public void Move()
+    {
+        Vector3 roamingPos = getRoamingPosition();
+        while(!transform.position.Equals(roamingPos))
+        {
+            Debug.Log("moving"+movementSpeed);
+            transform.position+=new Vector3(movementSpeed * Time.fixedDeltaTime,0,0);
+        }
+        
+    }
+
+    protected IEnumerator allowMove()
+    {
+        shouldMove = true;
+        yield return new WaitForSeconds(1);
+        shouldMove = false;
+    }
+}
---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 1;
+    public float movementSpeed = 1f;
+    public int jumpForce = 150;
+    public GameObject deathEffect;
+    public bool shouldMove = true;
+    public CharacterController2D controller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Take damage function
+    public void TakeDamage (int damage)
+    {
+
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected static Vector3 getRandomDirection()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1,1), UnityEngine.Random.Range(-1f,1f)).normalized;
+    }
+
+    protected static int getRandomHorizontalDirection()
+    {
+        if(UnityEngine.Random.Range(-1,1) <= 0) return -1;
+        else return 1;
+    }
+
+    // Die (virtual) function
+    virtual protected void Die()
+    {
+        //Debug.Log("dead: " + gameObject);
+        // Update the game controller that this person has died
+        // if (controller != null)
+        // {
+        //     TopDownController ControllerScript = Controller.GetComponent<TopDownController>();
+        //     if (ControllerScript != null && ControllerScript.numEnemiesLeft != 0)
+        //     {
+        //         ControllerScript.numEnemiesLeft--;
+        //         //Debug.Log(ControllerScript.numOfEnemiesInStage - ControllerScript.numEnemiesLeft);
+        //     }
+        // }
+
+        // Create death effect and destroy this object
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
