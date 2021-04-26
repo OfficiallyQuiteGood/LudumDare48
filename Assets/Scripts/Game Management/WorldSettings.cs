bcad7aed47b23904329424d35bbb5ac0b3e5d10c
Julien Collins <julienmcollins@gmail.com>
2021-04-26 10:01:03 -0700

    Adding ending music logic

--- Assets/Scripts/Game Management/WorldSettings.cs
+++ Assets/Scripts/Game Management/WorldSettings.cs
@@ -12,6 +12,7 @@ public class WorldSettings : MonoBehaviour
 
     public AudioSource audioSource;
     public AudioSource EnemyAudio;
+    public AudioSource endingMusicSource;
     public AudioClip[] audioClipArray;
     // Audio Clips
     //0
@@ -24,6 +25,9 @@ public class WorldSettings : MonoBehaviour
     public AudioClip[] damageNoises;
     public List<AudioClip[]> noisePacks;
     protected bool[] canPlay;
+    public bool reachedEnd = false;
+    private AudioClip currClip;
+    public AudioClip endingMusic;
 
 
     // Start is called before the first frame update
@@ -43,6 +47,19 @@ public class WorldSettings : MonoBehaviour
         noisePacks.Add(moveNoises);
         noisePacks.Add(damageNoises);
 
+    }
+
+    protected IEnumerator PlayBackgroundMusic()
+    {
+        while(!reachedEnd)
+        {
+            // Play random clip
+            currClip = RandomClip();
+            audioSource.PlayOneShot(currClip);
+
+            // Yield wait for song to be over
+            yield return new WaitForSeconds(currClip.length);
+        }
     }
 
     public void PlayNoise(int ind, float delay)
@@ -65,6 +82,20 @@ public class WorldSettings : MonoBehaviour
     AudioClip RandomClip()
     {
         return audioClipArray[Random.Range(0, audioClipArray.Length)];
+    }
+
+    public IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
     }
 
     // Update is called once per frame
@@ -95,5 +126,20 @@ public class WorldSettings : MonoBehaviour
     public void GameOver()
     {
 
+    }
+
+    public void OnWin()
+    {
+        // Tell it that we've reached the end
+        reachedEnd = true;
+
+        // Fade curr music out
+        StartFade(audioSource, 1.5f, 0.0f);
+
+        // Play one shot clip
+        endingMusicSource.PlayOneShot(endingMusic);
+
+        // Fade back in
+        StartFade(endingMusicSource, 1.5f, 0.202f);
     }
 }
