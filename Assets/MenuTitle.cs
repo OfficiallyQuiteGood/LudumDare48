3a74f85586992ed43a2f59c31b9bbd2f46172161
freddypage <fredlepage782@gmail.com>
2021-04-26 20:08:00 -0400

    menu intro added

---
+++ Assets/MenuTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        StartCoroutine(enableDelay(5));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator enableDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.GetComponent<Renderer>().enabled = true;
+    }
+}
--- Assets/Scripts/Game Management/MainMenu.cs
+++ Assets/Scripts/Game Management/MainMenu.cs
@@ -2,22 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Image IntroScreen;
+    bool isDone;
+    public AudioClip introMusic;
+    public AudioSource source;
     void Start()
     {
-        
+        if(source!=null && introMusic!=null) source.PlayOneShot(introMusic);
+        StartCoroutine(FadeInImage(true, 2, 1f));
+        StartCoroutine(FadeInImage(false, 2, 3.7f));
+        //gameObject.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (isDone && Input.anyKey)
         {
             Debug.Log("Game Start");
             SceneManager.LoadScene("TreeScene");
+        }
+    }
+
+    public IEnumerator FadeInImage(bool fadeIn = true, int fadeSpeed = 5,float delay = 0)
+    {
+        yield return new WaitForSeconds(delay);
+        Color objectColor = IntroScreen.color;
+        float fadeAmount;
+
+        if(fadeIn)
+        {
+            while(IntroScreen.color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                IntroScreen.color = objectColor;
+                yield return new WaitForSeconds(0.003f);
+            }
+        }
+        else
+        {
+            while(IntroScreen.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                IntroScreen.color = objectColor;
+                yield return new WaitForSeconds(0.003f);
+            }
+            isDone = true;
         }
     }
 }
