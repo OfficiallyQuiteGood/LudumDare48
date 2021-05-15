89d26ddc458c9ecd001f4cd1dfa3b0e6da876812
freddypage <fredlepage782@gmail.com>
2021-05-15 15:50:08 -0400

    added pause screen

---
+++ Assets/Scripts/Game Management/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isPaused = false;
+    void Start()
+    {
+        setActiveChildren(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape key was pressed");
+            if(isPaused) ResumeGame();
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame ()
+    {
+        Time.timeScale = 0;
+        setActiveChildren(true);
+    }
+
+    void ResumeGame ()
+    {
+        Time.timeScale = 1;
+        setActiveChildren(false);
+    }
+
+    void setActiveChildren(bool isActive)
+    {
+        for(int i=0; i<gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(isActive);
+        }
+    }
+}
---
+++ Assets/Scripts/Game Management/VolumeSlider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string volumeParameter = "Master";
+    public AudioMixer mixer;
+    public Slider slider;
+    private float multiplier = 30f;
+
+    private void Awake()
+    {
+        slider.onValueChanged.AddListener(HandleSliderChanged);
+    }
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(volumeParameter, slider.value);
+    }
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat(volumeParameter, slider.value);
+    }
+
+    private void HandleSliderChanged(float value)
+    {
+        mixer.SetFloat(volumeParameter, Mathf.Log10(value) * multiplier);
+        Debug.Log(Mathf.Log(value) * multiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
