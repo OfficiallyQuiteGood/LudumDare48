f14d340f34ca42cb388d7d7a97df1d84eebc7dae
freddypage <fredlepage782@gmail.com>
2021-05-15 19:23:40 -0400

    health ui changes + timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timeText;
+    float timeToDisplay = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timeToDisplay += 1;
+        DisplayTime();
+    }
+
+    void DisplayTime()
+    {
+        float seconds = Mathf.FloorToInt(timeToDisplay / 60 % 60);  
+        float milliseconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float minutes = Mathf.FloorToInt(timeToDisplay / (60*60));
+
+        timeText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliseconds);
+    }
+}
