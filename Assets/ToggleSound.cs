864b8d7fb53f04f754c8833996fa0590d8c17b04
freddypage <fredlepage782@gmail.com>
2021-05-16 17:29:47 -0400

    added toggle button

---
+++ Assets/ToggleSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSound : MonoBehaviour
+{
+    public GameObject timerText;
+    public Toggle toggleUI;
+    public bool timerIsEnabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleUI.onValueChanged.AddListener(delegate {
+            timerIsEnabled = !timerIsEnabled;
+            if(!timerIsEnabled) timerText.GetComponent<CanvasGroup>().alpha = 0;
+            else
+            {
+                timerText.GetComponent<CanvasGroup>().alpha = 1;
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
